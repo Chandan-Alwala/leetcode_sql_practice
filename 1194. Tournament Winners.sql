82180d8081abb66c2bb7686efede0195c7f0774a
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-02-14 08:41:12 -0600

    Update 1194. Tournament Winners.sql

--- 1194. Tournament Winners.sql
+++ 1194. Tournament Winners.sql
@@ -22,3 +22,37 @@ SELECT
 	FIRST_VALUE(cte_2.player) OVER(PARTITION BY p.group_id ORDER BY cte_2.total_score DESC) as player_id
 FROM cte_2
 LEFT JOIN players p ON p.player_id = cte_2.player;
+
+
+------------------------------
+
+
+WITH scores AS (
+    SELECT first_player AS player_id, first_score AS score
+    FROM Matches
+    UNION ALL
+    SELECT second_player, second_score
+    FROM Matches
+),
+total_scores AS (
+    SELECT 
+        p.player_id,
+        p.group_id,
+        SUM(COALESCE(s.score, 0)) AS total_score
+    FROM Players p
+    LEFT JOIN scores s
+        ON p.player_id = s.player_id
+    GROUP BY p.player_id, p.group_id
+)
+
+SELECT group_id, player_id
+FROM (
+    SELECT *,
+           RANK() OVER (
+               PARTITION BY group_id
+               ORDER BY total_score DESC, player_id ASC
+           ) AS rnk
+    FROM total_scores
+) t
+WHERE rnk = 1;
+
