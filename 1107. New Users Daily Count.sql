d852339772c995256e95f2afd030af34927d975a
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-02-14 08:38:05 -0600

    Update 1107. New Users Daily Count.sql

--- 1107. New Users Daily Count.sql
+++ 1107. New Users Daily Count.sql
@@ -15,6 +15,7 @@ FROM first_login
 WHERE DATEDIFF('2019-06-30', login_date) <= 90
 GROUP BY login_date;
 
+------------------------------------------
 
 # Solution with RANK or DENSE_RANK(both will work)
 WITH login_ranker AS (
@@ -34,3 +35,21 @@ FROM login_ranker
 WHERE l_rank = 1 AND DATEDIFF('2019-06-30', activity_date) <= 90
 GROUP BY activity_date
 ORDER BY activity_date;
+------------------------------------------
+
+WITH first_logins AS (
+    SELECT
+        user_id,
+        MIN(activity_date) AS first_login
+    FROM Traffic
+    WHERE activity = 'login'
+    GROUP BY user_id
+)
+SELECT
+    first_login AS login_date,
+    COUNT(*) AS user_count
+FROM first_logins
+WHERE first_login >= DATE_SUB('2019-06-30', INTERVAL 90 DAY)
+GROUP BY first_login
+ORDER BY first_login;
+
