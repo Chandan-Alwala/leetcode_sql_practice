c36934c3ef4e89f0efdc293a18e87884bc47ff7b
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-02-17 07:46:10 -0600

    Update 1270. All People Report to the Given Manager.sql

--- 1270. All People Report to the Given Manager.sql
+++ 1270. All People Report to the Given Manager.sql
@@ -4,3 +4,32 @@ FROM Employees e1
 JOIN Employees e2 ON e1.manager_id = e2.employee_id
 JOIN Employees e3 ON e2.manager_id = e3.employee_id
 WHERE e3.manager_id = 1 AND e1.employee_id != 1;
+
+--------------------
+
+WITH level1 AS (
+    SELECT employee_id
+    FROM Employees
+    WHERE manager_id = 1
+),
+
+level2 AS (
+    SELECT employee_id
+    FROM Employees
+    WHERE manager_id IN (SELECT employee_id FROM level1)
+),
+
+level3 AS (
+    SELECT employee_id
+    FROM Employees
+    WHERE manager_id IN (SELECT employee_id FROM level2)
+)
+
+SELECT DISTINCT employee_id
+FROM (
+    SELECT employee_id FROM level1
+    UNION ALL
+    SELECT employee_id FROM level2
+    UNION ALL
+    SELECT employee_id FROM level3
+) t;
