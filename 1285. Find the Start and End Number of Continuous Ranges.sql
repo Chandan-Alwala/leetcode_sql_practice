114527c9dffc88774245c9eafa95d741c9a33aae
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-02-17 08:56:12 -0600

    Update 1285. Find the Start and End Number of Continuous Ranges.sql

--- 1285. Find the Start and End Number of Continuous Ranges.sql
+++ 1285. Find the Start and End Number of Continuous Ranges.sql
@@ -1,3 +1,13 @@
+WITH numbered AS 
+    ( SELECT log_id, 
+    log_id - ROW_NUMBER() OVER (ORDER BY log_id) AS diff 
+FROM Logs ) 
+SELECT MIN(log_id) AS start_id, 
+    MAX(log_id) AS end_id 
+    FROM numbered 
+    GROUP BY diff ORDER BY start_id;
+---------------------------------------
+
 WITH log_ranker AS (
     SELECT
         log_id,
@@ -10,6 +20,6 @@ SELECT
     MIN(l1.log_id) AS start_id,
     MAX(l2.log_id) AS end_id
 FROM log_ranker l1
-JOIN log_ranker l2 ON l1.diff = l2.diff
+JOIN log_ranker l2 ON l1.diff = l2.diff -- need not join
 GROUP BY l1.diff
 ORDER BY start_id;
