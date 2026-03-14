936111923137954e2a88de61bf7d79adc4ecd055
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-14 18:35:36 -0500

    Optimize Top Travellers query with CTE
    Refactor SQL query to use CTE for calculating travelled distance.

--- 1407. Top Travellers.sql
+++ 1407. Top Travellers.sql
@@ -1,3 +1,15 @@
+With cte as (
+Select user_id, sum(distance) as travelled_distance
+From Rides 
+Group by 1 
+)
+
+Select 
+U.name as name, coalesce(c.travelled_distance,0) as c.travelled_distance
+From Users u left join cte c
+On u.id = c.user_id 
+Order by travelled_distance desc, name asc
+-------------------------------------
 SELECT
     u.name,
     IFNULL(SUM(r.distance), 0) AS travelled_distance
