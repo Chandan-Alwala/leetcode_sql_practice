9799ef305376a068e9c1b5cca0c6157aad94d542
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-14 19:11:19 -0500

    Fix SQL query by removing GROUP BY clause
    Removed unnecessary GROUP BY clause from the query.

--- 1421. NPV Queries.sql
+++ 1421. NPV Queries.sql
@@ -1,7 +1,15 @@
+SELECT 
+    q.id,
+    q.year,
+    COALESCE(n.npv, 0) AS npv
+FROM Queries q
+LEFT JOIN NPV n
+    ON q.id = n.id AND q.year = n.year;
+---------------------
 SELECT
     q.id,
     q.year,
     IFNULL(n.npv, 0) AS npv
 FROM queries q
 LEFT JOIN npv n ON q.id = n.id AND q.year = n.year
-GROUP BY q.id, q.year;
+GROUP BY q.id, q.year; --wrong, no need of group by 
