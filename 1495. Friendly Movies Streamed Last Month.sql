c2f2bacfe70a1b0c1ce6165243b2a6f8f6165358
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-16 10:21:25 -0500

    Refactor SQL queries for movie selection

--- 1495. Friendly Movies Streamed Last Month.sql
+++ 1495. Friendly Movies Streamed Last Month.sql
@@ -1,3 +1,14 @@
+# Without GROUP BY and distinct
+SELECT
+    DISTINCT c.title
+FROM TVProgram t
+LEFT JOIN content c ON t.content_id = c.content_id
+WHERE 
+    c.Kids_content = 'Y' AND 
+    c.content_type = 'Movies' AND 
+    MONTH(t.program_date) = 6 AND YEAR(t.program_date) = 2020
+ORDER BY c.title;
+-----------------------------------
 # With GROUP BY, no distinct
 SELECT
     c.title
@@ -8,16 +19,4 @@ WHERE
     c.content_type = 'Movies' AND 
     MONTH(t.program_date) = 6 AND YEAR(t.program_date) = 2020
 GROUP BY c.title
-ORDER BY c.title;
-
-
-# Without GROUP BY and distinct
-SELECT
-    DISTINCT c.title
-FROM TVProgram t
-LEFT JOIN content c ON t.content_id = c.content_id
-WHERE 
-    c.Kids_content = 'Y' AND 
-    c.content_type = 'Movies' AND 
-    MONTH(t.program_date) = 6 AND YEAR(t.program_date) = 2020
 ORDER BY c.title;
