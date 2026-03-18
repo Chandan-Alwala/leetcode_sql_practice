a3d9f400432fe73a8d1f0d36d95614ff7da240cd
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-18 07:53:05 -0500

    Use CTE to calculate account balances
    Refactor query to use CTE for balance calculation.

--- 1587. Bank Account Summary II.sql
+++ 1587. Bank Account Summary II.sql
@@ -1,3 +1,19 @@
+WITH balance_cte AS (
+    SELECT 
+        account,
+        SUM(amount) AS balance
+    FROM Transactions
+    GROUP BY account
+)
+
+SELECT 
+    u.name,
+    b.balance
+FROM Users u
+JOIN balance_cte b
+    ON u.account = b.account
+WHERE b.balance > 10000;
+-------------------------------
 SELECT
     u.name,
     SUM(t.amount) as balance
