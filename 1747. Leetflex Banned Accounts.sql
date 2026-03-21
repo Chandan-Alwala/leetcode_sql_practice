4f01d2701f864e106d098c64713d30fedd1e909e
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-21 17:15:14 -0500

    Update 1747. Leetflex Banned Accounts.sql

--- 1747. Leetflex Banned Accounts.sql
+++ 1747. Leetflex Banned Accounts.sql
@@ -1,3 +1,11 @@
+SELECT DISTINCT l1.account_id
+FROM LogInfo l1
+JOIN LogInfo l2
+    ON l1.account_id = l2.account_id
+   AND l1.ip_address <> l2.ip_address
+   AND l1.login <= l2.logout
+   AND l2.login <= l1.logout;
+--------------------------------
 # Solution with CTE
 WITH login_counter AS (
     SELECT
