ec0166329f9666519607b7c37d21a1ba83ef2d16
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-22 09:17:41 -0500

    Update 1789. Primary Department for Each Employee.sql

--- 1789. Primary Department for Each Employee.sql
+++ 1789. Primary Department for Each Employee.sql
@@ -1,3 +1,38 @@
+SELECT 
+    employee_id,
+    department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+
+UNION
+
+SELECT 
+    employee_id,
+    department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(*) = 1;
+--------
+SELECT 
+    employee_id,
+    department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+   OR employee_id IN (
+       SELECT employee_id
+       FROM Employee
+       GROUP BY employee_id
+       HAVING COUNT(*) = 1
+   );
+-------
+SELECT employee_id, department_id
+FROM (
+    SELECT *,
+           COUNT(*) OVER (PARTITION BY employee_id) AS cnt
+    FROM Employee
+) t
+WHERE primary_flag = 'Y' OR cnt = 1;
+----------------------------------
 # With sub-query and RANK
 SELECT
     employee_id,
