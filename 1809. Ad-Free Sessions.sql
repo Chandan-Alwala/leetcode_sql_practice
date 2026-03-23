d90979b2696814ce9362718785b0639335a4e110
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-23 11:22:49 -0500

    Update 1809. Ad-Free Sessions.sql

--- 1809. Ad-Free Sessions.sql
+++ 1809. Ad-Free Sessions.sql
@@ -1,3 +1,30 @@
+----------------------
+SELECT p.session_id
+FROM Playback p
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Ads a
+    WHERE a.customer_id = p.customer_id
+      AND a.timestamp BETWEEN p.start_time AND p.end_time
+);
+----------------------------
+--wrong 
+SELECT DISTINCT p.session_id
+FROM Playback p
+JOIN Ads a 
+  ON p.customer_id = a.customer_id
+ AND a.timestamp NOT BETWEEN p.start_time AND p.end_time; (***wrong)
+
+--correct
+SELECT p.session_id
+FROM Playback p
+LEFT JOIN Ads a
+  ON p.customer_id = a.customer_id
+ AND a.timestamp BETWEEN p.start_time AND p.end_time
+WHERE a.ad_id IS NULL;
+
+
+----------------------------
 SELECT
     session_id
 FROM playback
