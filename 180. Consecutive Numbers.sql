732b36cbf0f99c2e123afc9ae2e7ae8915b799ee
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-23 09:46:32 -0500

    Update 180. Consecutive Numbers.sql

--- 180. Consecutive Numbers.sql
+++ 180. Consecutive Numbers.sql
@@ -1,3 +1,14 @@
+WITH cte AS (
+    SELECT 
+        Num,
+        LAG(Num, 1) OVER (ORDER BY Id) AS prev1,
+        LAG(Num, 2) OVER (ORDER BY Id) AS prev2
+    FROM Logs
+)
+SELECT DISTINCT Num AS ConsecutiveNums
+FROM cte
+WHERE Num = prev1 AND Num = prev2;
+---------------------
 # Without LEAD/LAG, two joins
 SELECT
     DISTINCT c.num AS ConsecutiveNums
