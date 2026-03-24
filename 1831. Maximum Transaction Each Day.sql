20c1fc8ecf0202e8e36999258144f88998cdd575
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-24 08:05:57 -0500

    Update 1831. Maximum Transaction Each Day.sql

--- 1831. Maximum Transaction Each Day.sql
+++ 1831. Maximum Transaction Each Day.sql
@@ -1,3 +1,16 @@
+SELECT t.transaction_id
+FROM Transactions t
+JOIN (
+    SELECT 
+        DATE(day) AS d,
+        MAX(amount) AS max_amt
+    FROM Transactions
+    GROUP BY DATE(day)
+) m
+ON DATE(t.day) = m.d
+AND t.amount = m.max_amt
+ORDER BY t.transaction_id;
+----------------------
 WITH transaction_ranker AS(
     SELECT
         transaction_id,
