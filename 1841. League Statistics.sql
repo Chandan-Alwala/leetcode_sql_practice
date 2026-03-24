070f0578dea669632a91b4bcd66fad13a744c253
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-24 12:12:37 -0500

    Update 1841. League Statistics.sql

--- 1841. League Statistics.sql
+++ 1841. League Statistics.sql
@@ -1,3 +1,50 @@
+WITH all_matches AS (
+    SELECT 
+        home_team_id AS team_id,
+        home_team_goals AS goals_for,
+        away_team_goals AS goals_against
+    FROM Matches
+    
+    UNION ALL
+    
+    SELECT 
+        away_team_id AS team_id,
+        away_team_goals AS goals_for,
+        home_team_goals AS goals_against
+    FROM Matches
+),
+team_stats AS (
+    SELECT 
+        team_id,
+        COUNT(*) AS matches_played,
+        SUM(
+            CASE 
+                WHEN goals_for > goals_against THEN 3
+                WHEN goals_for = goals_against THEN 1
+                ELSE 0
+            END
+        ) AS points,
+        SUM(goals_for) AS goal_for,
+        SUM(goals_against) AS goal_against
+    FROM all_matches
+    GROUP BY team_id
+)
+
+SELECT 
+    t.team_name,
+    ts.matches_played,
+    ts.points,
+    ts.goal_for,
+    ts.goal_against,
+    (ts.goal_for - ts.goal_against) AS goal_diff
+FROM team_stats ts
+JOIN Teams t
+    ON t.team_id = ts.team_id
+ORDER BY 
+    ts.points DESC,
+    goal_diff DESC,
+    t.team_name;
+------------------------
 WITH match_pivot AS(
     SELECT home_team_id as team_id, home_team_goals, away_team_goals FROM matches
     UNION ALL
