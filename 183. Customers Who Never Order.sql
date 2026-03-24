2b41f7f4efcb11d7db55b2d6fb3c983711d3c7b1
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-24 07:54:08 -0500

    Update 183. Customers Who Never Order.sql

--- 183. Customers Who Never Order.sql
+++ 183. Customers Who Never Order.sql
@@ -1,3 +1,11 @@
+SELECT Name AS Customers
+FROM Customers
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Orders
+    WHERE Orders.CustomerId = Customers.Id
+);
+-----------------
 SELECT
     name AS Customers
 FROM customers 
