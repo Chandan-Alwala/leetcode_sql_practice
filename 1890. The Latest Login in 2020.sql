a29c029cb11e1f3985ddbb75500f47949b2cbc42
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-26 08:11:12 -0500

    Update 1890. The Latest Login in 2020.sql

--- 1890. The Latest Login in 2020.sql
+++ 1890. The Latest Login in 2020.sql
@@ -2,7 +2,7 @@ SELECT
     user_id,
     MAX(time_stamp) AS last_stamp
 FROM Logins
-WHERE YEAR(time_stamp) = 2020
+WHERE time_stamp >= '2020-01-01' AND time_stamp < '2021-01-01'
 GROUP BY user_id;
 -----------------------
 SELECT
