c12a223d4801de64e69bb113b3672194efae4ff9
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-27 08:33:19 -0500

    Update 1949. Strong Friendship.sql

--- 1949. Strong Friendship.sql
+++ 1949. Strong Friendship.sql
@@ -1,3 +1,31 @@
+WITH all_friends AS (
+    -- Make friendships bidirectional
+    SELECT user1_id AS user, user2_id AS friend FROM Friendship
+    UNION ALL
+    SELECT user2_id AS user, user1_id AS friend FROM Friendship
+),
+common_friends AS (
+    -- Find common friends for each pair
+    SELECT 
+        f1.user AS user1_id,
+        f2.user AS user2_id,
+        f1.friend AS common_friend
+    FROM all_friends f1
+    JOIN all_friends f2
+        ON f1.friend = f2.friend
+       AND f1.user < f2.user   -- avoid duplicates
+)
+SELECT 
+    f.user1_id,
+    f.user2_id,
+    COUNT(*) AS common_friend
+FROM common_friends cf
+JOIN Friendship f
+    ON cf.user1_id = f.user1_id
+   AND cf.user2_id = f.user2_id
+GROUP BY f.user1_id, f.user2_id
+HAVING COUNT(*) >= 3;
+-----------------------
 # Solution with CTEs and sub-query in final part
 WITH friends AS (
     SELECT user1_id AS user1_id, user2_id AS user2_id FROM Friendship
