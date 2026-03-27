d6de14acb76774e44f269910dea6f369b7637736
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-27 08:04:38 -0500

    Update 1939. Users That Actively Request Confirmation Messages.sql

--- 1939. Users That Actively Request Confirmation Messages.sql
+++ 1939. Users That Actively Request Confirmation Messages.sql
@@ -1,3 +1,24 @@
+WITH cte AS (
+    SELECT 
+        user_id,
+        time_stamp,
+        LEAD(time_stamp) OVER (
+            PARTITION BY user_id 
+            ORDER BY time_stamp
+        ) AS next_time
+    FROM Confirmations
+)
+SELECT DISTINCT user_id
+FROM cte
+WHERE TIMESTAMPDIFF(SECOND, time_stamp, next_time) <= 24 * 60 * 60;
+----------------------
+SELECT DISTINCT c1.user_id
+FROM Confirmations c1
+JOIN Confirmations c2
+  ON c1.user_id = c2.user_id
+ AND c1.time_stamp < c2.time_stamp
+ AND TIMESTAMPDIFF(SECOND, c1.time_stamp, c2.time_stamp) <= 24 * 60 * 60;
+----------------------
 SELECT
     DISTINCT s.user_id
 FROM confirmations s
