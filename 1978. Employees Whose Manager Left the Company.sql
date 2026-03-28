f242aa332748c68d96c9cd9b24c64792fd038d8b
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-03-28 09:24:25 -0500

    Add SQL query for employees with no manager
    Add query to select employees with left managers.

--- 1978. Employees Whose Manager Left the Company.sql
+++ 1978. Employees Whose Manager Left the Company.sql
@@ -1,3 +1,14 @@
+SELECT e.employee_id
+FROM Employees e
+WHERE e.salary < 30000
+AND e.manager_id IS NOT NULL
+AND NOT EXISTS (
+    SELECT 1
+    FROM Employees m
+    WHERE m.employee_id = e.manager_id
+)
+ORDER BY e.employee_id;
+-------------------------
 SELECT
     e1.employee_id
 FROM employees e1
