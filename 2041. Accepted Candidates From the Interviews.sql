2a3c9561cbd644c5f804f3881ed9d7fa7f58ad39
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-04-02 10:53:57 -0500

    Refactor query to use CTE for candidate selection
    Added a Common Table Expression (CTE) to filter candidates with at least 2 years of experience and a total score greater than 15.

--- 2041. Accepted Candidates From the Interviews.sql
+++ 2041. Accepted Candidates From the Interviews.sql
@@ -1,3 +1,17 @@
+WITH cte AS (
+    SELECT 
+        c.candidate_id,
+        r.score
+    FROM Candidates c
+    JOIN Rounds r
+      ON c.interview_id = r.interview_id
+    WHERE c.years_of_exp >= 2
+)
+SELECT candidate_id
+FROM cte
+GROUP BY candidate_id
+HAVING SUM(score) > 15;
+----------------------
 SELECT
     c.candidate_id
 FROM candidates c
