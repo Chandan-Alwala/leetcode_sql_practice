f8556cd001786262fd027c65237132b08da7470d
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-04-03 15:53:22 -0500

    Add query to drop Type 1 orders for Type 0 customers
    Added a query to select orders for customers without Type 0 orders.

--- 2084. Drop Type 1 Orders for Customers With Type 0 Orders.sql
+++ 2084. Drop Type 1 Orders for Customers With Type 0 Orders.sql
@@ -1,3 +1,13 @@
+SELECT *
+FROM Orders o
+WHERE o.order_type = 0
+   OR NOT EXISTS (
+        SELECT 1
+        FROM Orders o2
+        WHERE o2.customer_id = o.customer_id
+          AND o2.order_type = 0
+   );
+---------------
 WITH flag AS (
     SELECT 
         customer_id,
