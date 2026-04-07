3bf6033c515f4b49e39952d9716b3e66c3dde402
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-04-07 14:55:56 -0500

    Change RANK to ROW_NUMBER for gender ranking
    Refactor SQL query to use ROW_NUMBER instead of RANK for gender ranking.

--- 2308. Arrange Table by Gender.sql
+++ 2308. Arrange Table by Gender.sql
@@ -2,6 +2,25 @@ WITH gender_rank AS (
     SELECT
         user_id,
         gender,
+        ROW_NUMBER() OVER(PARTITION BY gender ORDER BY user_id) AS g_rank
+    FROM Genders
+)
+SELECT 
+    user_id,
+    gender
+FROM gender_rank
+ORDER BY 
+    g_rank,
+    CASE 
+        WHEN gender = 'female' THEN 1
+        WHEN gender = 'other' THEN 2
+        WHEN gender = 'male' THEN 3
+    END;
+-----------------
+WITH gender_rank AS (
+    SELECT
+        user_id,
+        gender,
         RANK() OVER(PARTITION BY gender ORDER BY user_id) AS g_rank
     FROM genders
 )
