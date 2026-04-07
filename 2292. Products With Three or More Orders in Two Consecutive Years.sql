70e71094a74a8a120f6a1a4ed3cf8338a5f7cf01
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-04-07 00:07:05 -0500

    Implement logic to find products with orders in consecutive years
    Added a CTE to count products with three or more orders in consecutive years.

--- 2292. Products With Three or More Orders in Two Consecutive Years.sql
+++ 2292. Products With Three or More Orders in Two Consecutive Years.sql
@@ -1,3 +1,26 @@
+WITH yearly_cnt AS (
+    SELECT 
+        product_id,
+        YEAR(purchase_date) AS yr,
+        COUNT(*) AS cnt
+    FROM Orders
+    GROUP BY product_id, YEAR(purchase_date)
+    HAVING COUNT(*) >= 3
+),
+check_consecutive AS (
+    SELECT 
+        product_id,
+        yr,
+        LEAD(yr) OVER (
+            PARTITION BY product_id 
+            ORDER BY yr
+        ) AS next_yr
+    FROM yearly_cnt
+)
+SELECT DISTINCT product_id
+FROM check_consecutive
+WHERE next_yr = yr + 1;
+-----------------
 WITH yearly_products_cnt AS (
     SELECT
         product_id,
