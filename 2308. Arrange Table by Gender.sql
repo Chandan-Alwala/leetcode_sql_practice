87816d0882539423d90031b9471519ac6c0d3009
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2026-04-07 14:56:24 -0500

    Simplify SQL query by removing gender ranking
    Removed the CTE for gender ranking from the SQL query.

--- 2308. Arrange Table by Gender.sql
+++ 2308. Arrange Table by Gender.sql
@@ -17,16 +17,4 @@ ORDER BY
         WHEN gender = 'male' THEN 3
     END;
 -----------------
-WITH gender_rank AS (
-    SELECT
-        user_id,
-        gender,
-        RANK() OVER(PARTITION BY gender ORDER BY user_id) AS g_rank
-    FROM genders
-)
 
-SELECT 
-    user_id,
-    gender
-FROM gender_rank
-ORDER BY g_rank, gender;
